151270bbbe2fcc7aafaad7025099ce1c09c47581
qqm <maomaoiii@qq.com>
2025-04-17 18:12:10 +0800

    init

--- example/build_jni/java/run-java.sh
+++ example/build_jni/java/run-java.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 javac -d target/classes -cp  ".:./jar/fastjson-1.2.83.jar" com/example/*.java && \
-java -Djava.library.path=../ -cp "target/classes:./jar/fastjson-1.2.83.jar" com.example.Main
+java -Djava.library.path=../ -cp "target/classes:./jar/fastjson-1.2.83.jar" com.example.Main -Xms2g -Xmx2g -Xss256k -XX:+UseG1GC

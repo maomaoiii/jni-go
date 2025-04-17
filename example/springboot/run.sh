5ec9ae5efd75558c4bcb94e654e88156b814a93e
bennysong <bennysong@tencent.com>
2025-04-18 01:02:18 +0800

    feat: springboot

--- example/springboot/run.sh
+++ example/springboot/run.sh
@@ -1,7 +1,7 @@
 
 export GOEXPERIMENT=cgocheck2
-export GODEBUG=invalidptr=1,cgocheck=2,gctrace=1
-export GOGC=300
+export GODEBUG=invalidptr=1,cgocheck=2,gctrace=0
+export GOGC=100
 
 mvn clean package
 java -Djava.library.path=so -jar target/jni-go-0.0.1-SNAPSHOT.jar -Xms2g -Xmx2g -Xss256k -XX:+UseG1GC

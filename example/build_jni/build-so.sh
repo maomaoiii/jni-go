a48237ba065bb6f0be8364954b085db54bda4e89
qqm <maomaoiii@qq.com>
2025-04-17 20:20:52 +0800

    init

--- example/build_jni/build-so.sh
+++ example/build_jni/build-so.sh
@@ -6,4 +6,4 @@ ar rcs libjx.a jx.o
 export CGO_CFLAGS="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
 export CGO_LDFLAGS="-L$JAVA_HOME/lib/server -L$JAVA_HOME/jre/lib/amd64/server -L$(pwd) -ljvm -ljx"
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -buildmode=c-shared -o libnative-sign.so dispatch.go
+GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -buildmode=c-shared -o libnative-sign.so
--- example/build_jni/java/com/example/GoFunc.java
+++ example/build_jni/java/com/example/GoFunc.java
@@ -2,6 +2,8 @@ package com.example;
 public class GoFunc {
     static {
         System.loadLibrary("native-sign");
+        System.setProperty("GODEBUG", "invalidptr=1");
+        System.setProperty("GOEXPERIMENT", "cgocheck2");
     }
     public static native String Dispatch(String var0);
 }

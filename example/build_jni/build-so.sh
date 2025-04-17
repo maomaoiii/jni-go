e993abceb2666d65a6ce27627e64aa9c5210bead
qqm <maomaoiii@qq.com>
2025-04-17 19:21:31 +0800

    init

--- example/build_jni/build-so.sh
+++ example/build_jni/build-so.sh
@@ -4,6 +4,6 @@ gcc -c jx.c -o jx.o -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/
 ar rcs libjx.a jx.o
 
 export CGO_CFLAGS="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
-export CGO_LDFLAGS="-L$JAVA_HOME/lib/server -L$(pwd) -ljvm -ljx"
+export CGO_LDFLAGS="-L$JAVA_HOME/lib/server -L$JAVA_HOME/jre/lib/amd64/server -L$(pwd) -ljvm -ljx"
 
 GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -buildmode=c-shared -o libnative-sign.so dispatch.go

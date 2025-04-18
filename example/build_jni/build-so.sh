caa47d2bc1c0419352f4d96bc423889bbbcae06e
qqm <maomaoiii@qq.com>
2025-04-18 09:10:13 +0800

    init

--- example/build_jni/build-so.sh
+++ example/build_jni/build-so.sh
@@ -10,6 +10,6 @@ export CGO_LDFLAGS="-L$JAVA_HOME/lib/server -L$JAVA_HOME/jre/lib/amd64/server -L
 #GOOS=linux GOARCH=amd64 CGO_ENABLED=1 GODEBUG=invalidptr=1 GOEXPERIMENT=cgocheck2 GOGC=30 go build -buildmode=c-shared -o libnative-sign.so
 
 #go1.19
-GOOS=linux GOARCH=amd64 CGO_ENABLED=1 GODEBUG=invalidptr=1 GODEBUG=cgocheck=2 GOGC=30 go build -buildmode=c-shared -o libnative-sign.so
+GOOS=linux GOARCH=amd64 CGO_ENABLED=1 GODEBUG=invalidptr=1 GODEBUG=cgocheck=2 GOGC=30 go build -buildmode=c-shared -o libnative-sign.so  -buildvcs=false
 
 cp libnative-sign.so /usr/lib64
